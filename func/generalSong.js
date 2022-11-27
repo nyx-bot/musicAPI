fa4ef82cd48e19017e035029e982fe7956548cb3
Syl <hi@sylviiu.dev>
2022-11-27 06:03:05 -0600

    only parse if values exist to prevent crashing

--- func/generalSong.js
+++ func/generalSong.js
@@ -23,26 +23,26 @@ module.exports = ({
             if(r[0] && !r[0].title && r[0][0] && r[0][0].title) r = r[0]
             if(r.length > 0) results[service.split(`.`).slice(0, -1).join(`.`)] = r.map(result => {
                 //console.log(result)
-                if(artist) {
+                if(result && result.title && artist && result.artists) {
                     let lowercase = compareTwoStrings(`${title}`.toLowerCase(), result.title.toLowerCase()) + compareTwoStrings(`${artist}`.toLowerCase(), result.artists[0].toLowerCase()) * .4
                     let uppercase = compareTwoStrings(`${title}`, result.title) + compareTwoStrings(`${artist}`, result.artists[0]) * .4
                     result.similarity = lowercase > uppercase ? lowercase : uppercase
                     //console.log(`Similarity of ${title} & ${result.title} by ${artist} / ${result.artists[0]}: ${result.similarity}`)
-                } else {
+                    return result;
+                } else if(result && result.title) {
                     let lowercase = compareTwoStrings(`${title}`.toLowerCase(), result.title.toLowerCase() + result.artists[0].toLowerCase())
                     let uppercase = compareTwoStrings(`${title}`, result.title + result.artists[0])
                     result.similarity = lowercase > uppercase ? lowercase : uppercase
                     //console.log(`Similarity of ${title} & ${result.title}: ${result.similarity}`)
-                };
-
-                return result;
-            }).sort((a, b) => a.similarity < b.similarity ? 1 : -1);
+                    return result;
+                } else return undefined;
+            }).filter(a => a !== undefined).sort((a, b) => a.similarity < b.similarity ? 1 : -1);
             s.finish()
         })
     });
 
     threads.asyncReady(...threadNames).then(() => {
-        console.log(`Top results for ${artist || `-- no artist --`} ${title}: ${Object.entries(results).filter(r => r[0] != `top`).map(r => `\n| ${r[0].toUpperCase()}: ${r[1][0].artists[0]} - ${r[1][0].title} (${Math.round(r[1][0].similarity*100)}%)`).join(``)}`)
+        console.log(`Top results for ${artist || `-- no artist --`} ${title}: ${Object.entries(results).filter(r => r[0] != `top`).map(r => `\n| ${r[0].toUpperCase()}: ${r[1] && r[1][0] ? `${r[1][0].artists[0]} - ${r[1][0].title} (${Math.round(r[1][0].similarity*100)}%)` : `-- NO RESULTS --`}`).join(``)}`)
         results.top = Object.values(results).map(a => a[0]).sort((a, b) => a.similarity < b.similarity ? 1 : -1)[0]
         res(results)
     })
