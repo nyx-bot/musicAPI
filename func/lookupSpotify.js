624753d99d61d01fe4e21f7586a78a4f9485131d
Syl <hi@sylviiu.dev>
2022-12-30 21:04:33 -0600

    spotify playlist pagination

--- func/lookupSpotify.js
+++ func/lookupSpotify.js
@@ -1,5 +1,5 @@
 const api = (keys, endpoint, query) => new Promise(async res => {
-    require('superagent').get(`https://api.spotify.com/v1/${endpoint}/${query}`).set(`Authorization`, `Bearer ${keys.spotify}`).then(r => {
+    require('superagent').get(endpoint.includes(`api.spotify.com`) ? endpoint : `https://api.spotify.com/v1/${endpoint}/${query}`).set(`Authorization`, `Bearer ${keys.spotify}`).then(r => {
         if(r && r.body) {
             res({
                 response: r.body,
@@ -81,6 +81,20 @@ module.exports = ({
 
     if(result) {
         if(result.tracks) {
+            console.log(`Received ${result.tracks.items.length} tracks; ${result.tracks.next ? `there are more tracks to receive, doing so now!` : `there are no more tracks to receive, parsing now!`}`)
+
+            while (result.tracks.next) await new Promise(async res => {
+                const { response } = await api(keys, result.tracks.next);
+
+                result.tracks.items.push(...response.items);
+
+                result.tracks.next = response.next;
+
+                console.log(`Added ${response.items.length} tracks! (total ${result.tracks.items.length}) -- ${result.tracks.next ? `next still exists, adding more...` : `next no longer exists!`}`)
+
+                res()
+            })
+
             result.title = result.name;
             result.album = { images: result.images }
             if(result.owner && !result.artists) result.artists = [result.owner]
