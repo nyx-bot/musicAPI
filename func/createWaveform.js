0ed423b02e8afa3f07fcad7c857f5b5f35b93234
Syl <hi@sylviiu.dev>
2023-02-10 10:48:12 -0600

    cleeean

--- func/createWaveform.js
+++ func/createWaveform.js
@@ -31,181 +31,176 @@ const parseData = (obj, length) => {
     }
 }
 
-module.exports = ({id, location, info, length}) => {
-    let prom;
-    
-    prom = new Promise(async (resolve, rej) => {
-        if(!waveforms[id] && (!info || !info.waveform)) {
-            let returnObj = undefined;
+module.exports = ({id, location, info, length}) => new Promise(async (resolve, rej) => {
+    if(!waveforms[id] && (!info || !info.waveform)) {
+        let returnObj = undefined;
 
-            const waveformPromise = new Promise(async (res, r) => {
-                while(typeof returnObj == `undefined`) {
-                    await new Promise(r => setTimeout(r, 200))
-                };
+        const waveformPromise = new Promise(async (res, r) => {
+            while(typeof returnObj == `undefined`) {
+                await new Promise(r => setTimeout(r, 200))
+            };
 
-                if(typeof returnObj == `object`) {
-                    return res(returnObj)
-                } else rej(returnObj)
-            })
+            if(typeof returnObj == `object`) {
+                return res(returnObj)
+            } else r(returnObj)
+        })
 
-            waveforms[id] = waveformPromise;
-            if(info) waveforms[info.id] = waveformPromise;
-            if(info) waveforms[info.url] = waveformPromise;
-    
-            let spawn;
-    
-            let parsed = false;
-        
-            let output = ``;
-    
-            const closeHandler = (e) => {
-                if(output) {
-                    if(!parsed) {
-                        parsed = true;
-                        const d = parseData(output, length);
-                        waveforms[id] = d;
+        waveforms[id] = waveformPromise;
+        if(info) waveforms[info.id] = waveformPromise;
+        if(info) waveforms[info.url] = waveformPromise;
 
-                        returnObj = d;
+        let spawn;
+
+        let parsed = false;
     
-                        if(info) {
-                            waveforms[info.id] = d;
-                            waveforms[info.url] = d;
-    
-                            info.waveform = d;
-                        }
-    
-                        resolve(waveforms[id])
+        let output = ``;
+
+        const closeHandler = (e) => {
+            if(output) {
+                if(!parsed) {
+                    parsed = true;
+                    const d = parseData(output, length);
+                    waveforms[id] = d;
+
+                    returnObj = d;
+
+                    if(info) {
+                        waveforms[info.id] = d;
+                        waveforms[info.url] = d;
+
+                        info.waveform = d;
                     }
-                } else {
-                    const m = `Error occurred while running.`
-                    rej(m)
-                    returnObj = m;
-                    console.error(e)
+
+                    resolve(waveforms[id])
                 }
+            } else {
+                const m = `Error occurred while running.`
+                rej(m)
+                returnObj = m;
+                console.error(e)
             }
-    
-            const args = `--output-format json -b 8 -z 600 -q -o -`.split(` `)
-    
-            const methods = {
-                rawLocation: () => new Promise(async res => {
-                    console.log(`[WAVEFORM] USING FILE LOCATION FOR WAVEFORM`)
-                    return res(false)
-                    if(location) {
-                        try {
-                            spawn = require('child_process').spawn(`audiowaveform`, [`--input-format`, `mp3`, `-i`, location, ...args])
-            
-                            spawn.on(`error`, () => {
-                                console.warn(`Failed to get stream from raw audio file:`, e);
-                                res(false)
-                            })
-    
-                            spawn.stderr.on(`data`, d => {
-                                d.toString().split(`\n`).forEach(s => {
-                                    console.error(`[WAVEFORM/RAW]: ${s.toString().trim()}`)
-                                })
-                            })
-    
-                            spawn.stdout.on(`data`, d => {
-                                output = output + d.toString();
-                            });
-                        
-                            spawn.on(`close`, () => res(true));
-                        } catch(e) {
-                            console.warn(`Failed to get stream from raw audio file:`, e);
-                            res()
-                        }
-                    } else res()
-                }),
-                ffmpegProxy: () => new Promise(async res => {
-                    console.log(`[WAVEFORM] CREATING FFMPEG STREAM FOR WAVEFORM`)
-        
-                    spawn = require('child_process').spawn(`audiowaveform`, [`--input-format`, `mp3`, `-i`, `-`, ...args]);
-        
-                    const ffmpegArgs = [
-                        `-i`, location, 
-                        `-f`, `mp3`,
-                        `-ar`, `24000`,
-                        `-b:a`, `64k`,
-                        `-`
-                    ];
-    
-                    console.log(`[WAVEFORM] Spawning ffmpeg with args: "${ffmpegArgs.join(` `)}"`)
-    
-                    const stream = require('child_process').spawn(`ffmpeg`, ffmpegArgs);
-            
-                    stream.stdout.pipe(spawn.stdin);
-            
-                    stream.on(`close`, () => {
-                        spawn.stdin.destroy();
-                    });
-    
-                    spawn.stderr.on(`data`, d => {
-                        d.toString().split(`\n`).forEach(s => {
-                            console.error(`[WAVEFORM/FFMPEG]: ${s.toString().trim()}`)
-                        })
-                    })
-            
-                    spawn.on(`error`, () => res(false))
-    
-                    spawn.stdout.on(`data`, d => {
-                        output = output + d.toString();
-                    });
-                
-                    spawn.on(`close`, () => res(true));
-                }),
-                streamFromOwnApi: () => new Promise(async res => {
-                    console.log(`[WAVEFORM] CREATING STREAM FOR WAVEFORM`)
-        
-                    spawn = require('child_process').spawn(`audiowaveform`, [`--input-format`, `opus`, `-i`, `-`, ...args]);
-                    
-                    const stream = require(`superagent`).get(`http://127.0.0.1:1366/stream/${info ? info.url : id}`).set(`authorization`, require(`../config.json`).authKey)
-            
-                    stream.pipe(spawn.stdin);
-            
-                    stream.on(`close`, () => {
-                        spawn.stdin.destroy();
-                    });
-    
-                    spawn.stderr.on(`data`, d => {
-                        d.toString().split(`\n`).forEach(s => {
-                            console.error(`[WAVEFORM/API]: ${s.toString().trim()}`)
-                        })
-                    })
-            
-                    spawn.on(`error`, () => res(false))
-    
-                    spawn.stdout.on(`data`, d => {
-                        output = output + d.toString();
-                    });
-                
-                    spawn.on(`close`, () => res(true));
-                })
-            };
-    
-            for (let name of Object.keys(methods)) {
-                const m = methods[name]
-                if(!output) {
-                    const r = await m();
-                    if(r) {
-                        console.log(`${name} has returned with true! (Object string length: ${output.length})`);
-                    } else {
-                        console.log(`${name} has returned with false. (Object string length: ${output.length})`);
-                        await new Promise(r => setTimeout(() => {
-                            output = ``;
-                            r()
-                        }, 1000))
-                    };
-                }
-            };
-    
-            closeHandler();
-        } else if(waveforms[id]) {
-            if(waveforms[id].then) {
-                waveforms[id].then(resolve).catch(rej);
-            } else return resolve(waveforms[id])
-        } else if(info && info.waveform) {
-            return resolve(info.waveform)
         }
-    });
-    return prom;
-}
+
+        const args = `--output-format json -b 8 -z 600 -q -o -`.split(` `)
+
+        const methods = {
+            rawLocation: () => new Promise(async res => {
+                console.log(`[WAVEFORM] USING FILE LOCATION FOR WAVEFORM`)
+                return res(false)
+                if(location) {
+                    try {
+                        spawn = require('child_process').spawn(`audiowaveform`, [`--input-format`, `mp3`, `-i`, location, ...args])
+        
+                        spawn.on(`error`, () => {
+                            console.warn(`Failed to get stream from raw audio file:`, e);
+                            res(false)
+                        })
+
+                        spawn.stderr.on(`data`, d => {
+                            d.toString().split(`\n`).forEach(s => {
+                                console.error(`[WAVEFORM/RAW]: ${s.toString().trim()}`)
+                            })
+                        })
+
+                        spawn.stdout.on(`data`, d => {
+                            output = output + d.toString();
+                        });
+                    
+                        spawn.on(`close`, () => res(true));
+                    } catch(e) {
+                        console.warn(`Failed to get stream from raw audio file:`, e);
+                        res()
+                    }
+                } else res()
+            }),
+            ffmpegProxy: () => new Promise(async res => {
+                console.log(`[WAVEFORM] CREATING FFMPEG STREAM FOR WAVEFORM`)
+    
+                spawn = require('child_process').spawn(`audiowaveform`, [`--input-format`, `mp3`, `-i`, `-`, ...args]);
+    
+                const ffmpegArgs = [
+                    `-i`, location, 
+                    `-f`, `mp3`,
+                    `-ar`, `24000`,
+                    `-b:a`, `64k`,
+                    `-`
+                ];
+
+                console.log(`[WAVEFORM] Spawning ffmpeg with args: "${ffmpegArgs.join(` `)}"`)
+
+                const stream = require('child_process').spawn(`ffmpeg`, ffmpegArgs);
+        
+                stream.stdout.pipe(spawn.stdin);
+        
+                stream.on(`close`, () => {
+                    spawn.stdin.destroy();
+                });
+
+                spawn.stderr.on(`data`, d => {
+                    d.toString().split(`\n`).forEach(s => {
+                        console.error(`[WAVEFORM/FFMPEG]: ${s.toString().trim()}`)
+                    })
+                })
+        
+                spawn.on(`error`, () => res(false))
+
+                spawn.stdout.on(`data`, d => {
+                    output = output + d.toString();
+                });
+            
+                spawn.on(`close`, () => res(true));
+            }),
+            streamFromOwnApi: () => new Promise(async res => {
+                console.log(`[WAVEFORM] CREATING STREAM FOR WAVEFORM`)
+    
+                spawn = require('child_process').spawn(`audiowaveform`, [`--input-format`, `opus`, `-i`, `-`, ...args]);
+                
+                const stream = require(`superagent`).get(`http://127.0.0.1:1366/stream/${info ? info.url : id}`).set(`authorization`, require(`../config.json`).authKey)
+        
+                stream.pipe(spawn.stdin);
+        
+                stream.on(`close`, () => {
+                    spawn.stdin.destroy();
+                });
+
+                spawn.stderr.on(`data`, d => {
+                    d.toString().split(`\n`).forEach(s => {
+                        console.error(`[WAVEFORM/API]: ${s.toString().trim()}`)
+                    })
+                })
+        
+                spawn.on(`error`, () => res(false))
+
+                spawn.stdout.on(`data`, d => {
+                    output = output + d.toString();
+                });
+            
+                spawn.on(`close`, () => res(true));
+            })
+        };
+
+        for (let name of Object.keys(methods)) {
+            const m = methods[name]
+            if(!output) {
+                const r = await m();
+                if(r) {
+                    console.log(`${name} has returned with true! (Object string length: ${output.length})`);
+                } else {
+                    console.log(`${name} has returned with false. (Object string length: ${output.length})`);
+                    await new Promise(r => setTimeout(() => {
+                        output = ``;
+                        r()
+                    }, 1000))
+                };
+            }
+        };
+
+        closeHandler();
+    } else if(waveforms[id]) {
+        if(waveforms[id].then) {
+            waveforms[id].then(resolve).catch(rej);
+        } else return resolve(waveforms[id])
+    } else if(info && info.waveform) {
+        return resolve(info.waveform)
+    }
+});
